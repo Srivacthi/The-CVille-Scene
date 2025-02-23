1116eac07e831eae4494fa55e364e849615ec207
Srivacthi <Srivacthis@outlook.com>
2025-02-23 10:49:19 -0500

    Attempted to sort reviews by form

--- app.py
+++ app.py
@@ -70,21 +70,31 @@ def match_results():
 
 @app.route('/reviews', methods=['GET', 'POST'])
 def reviews():
+    reviews_sorted = reviews_table
+    sort_by = 'date'
     if request.method == 'POST':
-        #getting form data
-        rating = request.form['rating']
-        event_name = request.form['event_name']
-        description = request.form['description']
-        date = request.form['date']
-        #saving review
-        reviews_table.append({
-            'rating': rating,
-            'event_name': event_name,
-            'description': description,
-            'date': date
-        })
-        return redirect(url_for('reviews'))
-    return render_template('reviews.html', reviews_table=reviews_table)
+        action = request.form.get('action')
+        if action == 'sort':
+            sort_by = request.form.get('sort_by', 'rating') 
+            if sort_by == 'rating':
+                reviews_sorted = sorted(reviews_table, key=lambda x: x['rating'], reverse=True)
+            elif sort_by == 'date':
+                reviews_sorted = sorted(reviews_table, key=lambda x: x['date'], reverse=True)
+        elif action == 'submit_review':
+            #getting form data
+            rating = request.form['rating']
+            event_name = request.form['event_name']
+            description = request.form['description']
+            date = request.form['date']
+            #saving review
+            reviews_table.append({
+                'rating': rating,
+                'event_name': event_name,
+                'description': description,
+                'date': date
+            })
+            return redirect(url_for('reviews'))
+    return render_template('reviews.html', reviews_table=reviews_sorted, sort_by=sort_by)
 
 if __name__ == '__main__':
     app.run(debug=True)
